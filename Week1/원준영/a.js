4db02abd465d173cfac27bae49634f95b711f2c8
geg222 <gl021414@naver.com>
2025-03-25 19:51:35 +0900

    Create 1주차 원준영
    Create 1주차 원준영

---
+++ Week1/원준영/a.js
@@ -0,0 +1,9 @@
+function setDarkMode() {
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector("body").style.color = "white";
+}
+
+function setLightMode() {
+  document.querySelector("body").style.backgroundColor = "white";
+  document.querySelector("body").style.color = "black";
+}
