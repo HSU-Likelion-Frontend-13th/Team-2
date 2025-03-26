eb5ec3e9370bfa42bce76df8f3025c4ceb6c074a
9hkmo <gurah129@gmail.com>
2025-03-26 21:44:03 +0900

    [1주차] 구혁모  (#3)
    * ✨: 1주차 구혁모 생성
    
    * ✨: index.html 구현
    
    * ✨: 목록별 페이지 구현
    
    * ✨: 버튼 추가 + css 추가
    
    * ♻️: onClick 중복 함수 JS 파일 분리

---
+++ Week1/구혁모/fun.js
@@ -0,0 +1,8 @@
+function onClickDark() {
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.color = 'white';
+}
+function onClickLight() {
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+}
