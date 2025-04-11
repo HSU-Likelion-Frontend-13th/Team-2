620cf6f1c6843ef4b2846cc53c91da9a7ad229a4
9hkmo <gurah129@gmail.com>
2025-04-11 23:05:42 +0900

    ✨ Hobby 컴포넌트 생성

---
+++ Week4/hkmo/src/components/Hobby.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Title = styled.h1``;
+const Wrapper = styled.div``;
+const Card = styled.div``;
+const Top = styled.div``;
+
+export const Hobby = () => {
+  return (
+    <>
+      <Title></Title>
+      <Wrapper>
+        <Card>
+            <Top></Top>
+            <img src="" alt=""/>
+        </Card>
+        <Card>
+            <Top></Top>
+            <img src="" alt=""/>
+        </Card>
+        <Card>
+            <Top></Top>
+            <img src="" alt=""/>
+        </Card>
+      </Wrapper>
+    </>
+  );
+};
