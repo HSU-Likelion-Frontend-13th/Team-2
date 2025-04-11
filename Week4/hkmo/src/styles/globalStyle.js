f5add00a47dc6d4965d0b66c03c71a6dcfbe9f6a
9hkmo <gurah129@gmail.com>
2025-04-11 21:49:35 +0900

    💄globalstyle에 Interop 폰트 추가

--- Week4/hkmo/src/styles/globalStyle.js
+++ Week4/hkmo/src/styles/globalStyle.js
@@ -3,6 +3,20 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
+  @font-face {
+  font-family: "Interop";
+  src: url("https://raw.githubusercontent.com/payw-org/Interop/main/web/fonts/Interop-Regular.woff2")
+      format("woff2"),
+    url("https://raw.githubusercontent.com/payw-org/Interop/main/web/fonts/Interop-Regular.woff")
+      format("woff");
+  font-weight: normal;
+  font-style: normal;
+  font-display: block;
+}
+
+* {
+  font-family: "Interop";
+}
 `;
 
 export default GlobalStyle;
