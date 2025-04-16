0f9381ffaaf679b1eeaaef9e6e0afb3c39eb10e9
tpdus112 <tpdus6401@naver.com>
2025-04-16 22:37:08 +0900

    ✨ : About 추가

--- Week4/seyoun/src/App.js
+++ Week4/seyoun/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import About from './components/About'
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       <nav>
         <Nav />
       </nav>
+      <section>
+        <About />
+      </section>
     </div>
   );
 }
