129f24a7056e6507344e95b8b124061ee45204eb
구혁모 <gurah129@gmail.com>
2025-05-06 20:45:46 +0900

    🔥 불필요한 코드 삭제

--- Week7/hkmo/src/components/main/Main.style.jsx
+++ Week7/hkmo/src/components/main/Main.style.jsx
@@ -5,24 +5,11 @@ export const SectionTitle = styled.div`
   font-weight: 700;
 `;
 
-/* TodoComment */
-export const TodoListLayout = styled.div`
-  width: 60vw;
-`;
-
 export const AddCommentWrapper = styled.div`
   display: flex;
   gap: 0.5rem;
   justify-content: center;
   align-items: center;
-`;
-
-export const TodoListWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin: 1rem 0;
 `;
 
 export const CommentSection = styled.div`
@@ -48,6 +35,7 @@ export const ButtonLayout = styled.button`
   text-align: center;
   background-color: #5664f5;
 `;
+
 export const ButtonContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -57,27 +45,11 @@ export const ButtonContainer = styled.div`
   color: white;
 `;
 
-export const TodoItemLayout = styled.div`
-  width: 70%;
-  height: 2.8rem;
-  border: 1px solid gray;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.6rem;
-`;
-
-export const CheckboxImgContainer = styled.button``;
-export const CheckboxImg = styled.img``;
-
-export const TodoText = styled.div``;
-
 export const DeleteButton = styled.button`
   width: 50px;
   height: 30px;
   border: 1px solid grey;
 `;
-
-export const DeleteImg = styled.img``;
 
 export const CommentlistWrapper = styled.div`
   width: 100%;
@@ -102,8 +74,6 @@ export const CommentText = styled.div`
   gap: 0.5rem;
   align-items: center;
 `;
-
-export const DateText = styled.div``;
 
 export const EditButton = styled.button`
   width: 50px;
--- Week7/hkmo/src/components/main/comment/CommentList.jsx
+++ Week7/hkmo/src/components/main/comment/CommentList.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { useState } from "react";
 import CommentEditor from "./CommentEditor";
 import * as S from "../Main.style";
