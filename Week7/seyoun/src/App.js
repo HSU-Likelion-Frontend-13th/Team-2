a17eb07d6cb44599ca37b20c2914d6f525329d2c
tpdus112 <tpdus6401@naver.com>
2025-05-07 20:15:40 +0900

    💄 : 스타일 파일 별도 생성

--- Week7/seyoun/src/App.js
+++ Week7/seyoun/src/App.js
@@ -1,11 +1,15 @@
-import Header from './components/header/Header';
+import { GlobalStyle } from "./styles/globalStyles";
+import Main from "./pages/main/Main";
+import Header from "./components/header/Header";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-        </div>
-    );
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
--- Week7/seyoun/src/index.js
+++ Week7/seyoun/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 // root 엘리먼트를 찾아 React 18 버전 방식으로 렌더링할 준비
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<React.StrictMode>
-    <App/>
-</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
