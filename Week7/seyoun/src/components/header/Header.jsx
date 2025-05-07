94ee2c1028f67adb69597315e8414b557e9a7bcf
tpdus112 <tpdus6401@naver.com>
2025-05-07 20:05:45 +0900

    ✨ : Header 재 생성

--- Week7/seyoun/src/components/header/Header.jsx
+++ Week7/seyoun/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import * as S from './Header.style'; // Header 스타일 컴포넌트
-import Check from '../../assets/check.png'; // 체크 아이콘 이미지
+import * as S from "./Header.style";
+import CHECK from "../../assets/check.png";
 
 export default function Header() {
-    return (
-        <S.HeaderLayout>
-            <S.CheckIcon src={Check}/>
-            <S.Title>Comment</S.Title>
-        </S.HeaderLayout>
-    );
+  return (
+    <S.HeaderLayout>
+      <S.CheckIcon src={CHECK} />
+      <S.Title>Comment</S.Title>
+    </S.HeaderLayout>
+  );
 }
