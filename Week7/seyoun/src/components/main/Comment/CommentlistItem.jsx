a43092d41a02f72beb83a24fc0cf3c7f58ff5a25
tpdus112 <tpdus6401@naver.com>
2025-05-07 20:42:07 +0900

    ✨: 수정버튼 생성

--- Week7/seyoun/src/components/main/Comment/CommentlistItem.jsx
+++ Week7/seyoun/src/components/main/Comment/CommentlistItem.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import * as S from "../Main.style";
-import { useState } from "react";
 
 export default function CommentItem({commentText, commentlist, setCommentlist}) {
-    const [isChecked, setIschecked] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedText, setEditedText] = useState(commentText);
 
-    //삭제 함수
+    // 삭제 함수
     const deleteComment = () => {
         setCommentlist(commentlist.filter((comment) => comment !== commentText));
     };
-    
+
+    // 수정 완료 함수
+    const editComment = () => {
+        if (editedText.trim() === "") 
+            return;
+        const edited = commentlist.map(
+            (comment) => comment === commentText
+                ? editedText
+                : comment
+        );
+        setCommentlist(edited);
+        setIsEditing(false);
+    };
+
     return (
         <S.CommentItemLayout>
-                <S.CommentText>{commentText}</S.CommentText>
+            {
+                isEditing
+                    ? (
+                        <S.EditInput
+                            type="text"
+                            value={editedText}
+                            onChange={(e) => setEditedText(e.target.value)}/>
+                    )
+                    : (<S.CommentText>{commentText}</S.CommentText>)
+            }
+
             <S.DeleteButton onClick={deleteComment}>삭제</S.DeleteButton>
+
+            {
+                isEditing
+                    ? (<S.EditButton onClick={editComment}>수정완료</S.EditButton>)
+                    : (<S.EditButton onClick={() => setIsEditing(true)}>수정</S.EditButton>)
+            }
         </S.CommentItemLayout>
     );
 }
